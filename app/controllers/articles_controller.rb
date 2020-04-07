66915f8ba5164da5c669c334389d1a19c880179d
leannethng <leanne.thng@gmail.com>
2020-04-06 20:58:35 -0700

    Add - index page, artlcle page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,33 @@
 class ArticlesController < ApplicationController
   # $ rails routes, has predefined URI patterns such as articles#new with you can define here to hook into.
-  
+
+  def index
+    @articles = Article.all
+  end
+
+
+  def show
+    @article = Article.find(params[:id])
+  end 
+
+
   def new
   end
 
+
   def create
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+
     # When the submit button is clicked the page renders with the parameter key/values for each form field
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
   end
+
+  private
+  # the method is often made private to make sure it can't be called outside its intended context. 
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end  
 end
