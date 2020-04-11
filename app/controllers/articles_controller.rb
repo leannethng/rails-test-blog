6983e358bbf3600f10bce8f6ef3c201e69597c94
leannethng <leanne.thng@gmail.com>
2020-04-11 13:18:04 -0700

    Feature: Delete added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  # $ rails routes, has predefined URI patterns such as articles#new with you can define here to hook into.
+  # $ rails routes, has predefined URI patterns such as
+  # articles#new with you can define here to hook into.
 
   def index
     @articles = Article.all
@@ -7,39 +8,47 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
-  end 
+  end
 
   def new
     @article = Article.new
   end
 
-  def edit 
+  def edit
     @article = Article.find(params[:id])
-  end  
+  end
 
   def create
     @article = Article.new(article_params)
-   # if the save doesnt vaildated we render the form through 'new'
+    # if the save doesn't validate we render the form through 'new'
     if @article.save
-      redirect_to @article  
+      redirect_to @article
     else
-      render 'new'  
-    end  
+      render 'new'
+    end
   end
 
   def update
     @article = Article.find(params[:id])
-
+    # Redirect back to the show article
     if @article.update(article_params)
       redirect_to @article
     else
       render 'edit'
-    end  
-  end  
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    # redirecting back to the index
+    redirect_to articles_path
+  end
 
   private
-  # the method is often made private to make sure it can't be called outside its intended context. 
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end  
+  #  the method is often made private to make sure it can't be called
+  # outside it's intended context.
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
+# Defining the article
 class Article < ApplicationRecord
   # Data validation for title
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true, length: { minimum: 5 }
 end
